4c7a058c31835845e6c91233d8bd431c7e9bdff9
malzj <609658960@qq.com>
2016-06-01 14:37:54 +0800

    增加input 用来存储兴趣选项值

--- temp/query_caches/sqlcache_config_file_4adbe8489bb27571d5a562c339b09215.php
+++ temp/query_caches/sqlcache_config_file_4adbe8489bb27571d5a562c339b09215.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1464762471;
+$this->mysql_config_cache_file_time = 1464762968;
 $this->timeline = 77;
 $this->timezone = 28800;
 $this->platform = 'OTHER';

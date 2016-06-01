74a9e50483003a5b2db9d6d2235b42c695d5faed
malzj <609658960@qq.com>
2016-06-01 17:53:05 +0800

    安全中心设置判断

--- temp/query_caches/sqlcache_config_file_4adbe8489bb27571d5a562c339b09215.php
+++
@@ -1,6 +0,0 @@
-<?php
-$this->mysql_config_cache_file_time = 1464765517;
-$this->timeline = 77;
-$this->timezone = 28800;
-$this->platform = 'OTHER';
-?>
--- temp/query_caches/sqlcache_config_file_84ddd25ac9a397560bd2755bb8a62b2d.php
+++ temp/query_caches/sqlcache_config_file_84ddd25ac9a397560bd2755bb8a62b2d.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1464770525;
+$this->mysql_config_cache_file_time = 1464774593;
 $this->timeline = 85;
 $this->timezone = 28800;
 $this->platform = 'OTHER';

def97608fdf98276722b1076e99830eedf9e2985
malzj <609658960@qq.com>
2016-06-13 11:56:37 +0800

    3

--- jyflapi/Application/Games/Controller/GamesApiController.class.php
+++ jyflapi/Application/Games/Controller/GamesApiController.class.php
@@ -187,9 +187,6 @@ class GamesApiController extends Controller
             $wdata['lottery'] = $winner_num;
             $wdata['create_time'] = date('Y-m-d H:i:s',time());
             $wre = $Model ->table('__WINNERS_LIST__') -> data($wdata) -> add();
-            $userInfo = $Model->table('__USERS__')->where(array('user_name'=>$wdata['card_num']))->find();
-            $content = !empty($userInfo['nickname'])?$userInfo['nickname']:''."先生/女士，您的卡号".$wdata['card_num']."获得".$game_info['game_name']."第".
-                date('Ymd',strtotime($game_info['create_time']))."期奖品，中奖号码为".$winner_num."，请登录网站查看详细信息！";
             if($wre !== false){
                 $rudata['result'] = 'true';
                 $rudata['msg'] = '抢购成功！';
@@ -214,12 +211,19 @@ class GamesApiController extends Controller
             $jydata = $Card -> getResult();
             $Model -> table('__USERS__')->where('user_name')->data(array('card_money'=>$jydata['Points']))->save();
             $Model -> commit();
-            $Smsvrerify = new smsvrerifyApi();
+            if($count_all==$total) {
+                $Smsvrerify = new smsvrerifyApi();
+                $userInfo = $Model->table('__USERS__')->where(array('user_name'=>$wdata['card_num']))->find();
+                $content = (!empty($userInfo['nickname'])?$userInfo['nickname']:'')."先生/女士，您的卡号".$wdata['card_num']."获得".$game_info['game_name']."第".
+                    date('Ymd',strtotime($game_info['create_time']))."期奖品，中奖号码为".$winner_num."，请登录网站查看详细信息！";
 
+                $rudata['user']=$userInfo;
+                $rudata['content']=$content;
+                if(empty($userInfo['mobile_phone']))
+                $Smsvrerify->smsvrerify($userInfo['mobile_phone'],$content,0);
+            }
             $rudata['result'] = 'true';
             $rudata['msg'] = '抢购成功！';
-            $rudata['user']=$userInfo;
-            $rudata['content']=$content;
             $this -> ajaxReturn($rudata);
         }else{
             $Model -> rollback();

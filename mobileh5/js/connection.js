7c4d9c704557a3f969f781b9432ad0ff839b9582
malzj <609658960@qq.com>
2016-07-07 09:37:34 +0800

    文件重命名

--- mobileh5/js/connection.js
+++ mobileh5/js/connection.js
@@ -1,5 +1,6 @@
 /**
  * Created by chao on 2016/7/5.
  */
-// var api_url="http://www.juyoufuli.com";
+// var web_url="http://www.juyoufuli.com";
+var web_url="http://192.168.0.54";
 var api_url="http://jy.com";

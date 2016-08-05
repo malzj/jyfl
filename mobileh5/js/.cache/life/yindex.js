88038213633c6fe04daca1fe18ecd2ddc61c38ff
malzj <609658960@qq.com>
2016-08-05 11:43:43 +0800

    ..........

--- mobileh5/js/.cache/life/yindex.js
+++ mobileh5/js/.cache/life/yindex.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":23,"md5":"e650689382768ecf37cd74fd4984f070"}*/
+/*TMODJS:{"version":24,"md5":"e650689382768ecf37cd74fd4984f070"}*/
 template('life/yindex',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$each=$utils.$each,data=$data.data,banner=$data.banner,$index=$data.$index,$escape=$utils.$escape,i=$data.i,cate=$data.cate,attr=$data.attr,good=$data.good,$out='';$out+='<div class="mui-content">  <div id="slider" class="mui-slider" > <div class="mui-slider-group mui-slider-loop"> <div class="mui-slider-item mui-slider-item-duplicate slide-a"> <a href=""> <img src=""> </a> </div> ';

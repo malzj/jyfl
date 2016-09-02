5aa38ef198e0479ba6615921895932b2ac01e1e2
malzj <609658960@qq.com>
2016-09-02 15:10:07 +0800

    no message

--- mobileh5/js/.cache/flow/checkout.js
+++ mobileh5/js/.cache/flow/checkout.js
@@ -1,4 +1,4 @@
-/*TMODJS:{"version":402,"md5":"0866e7cc241c98a47c57f4538144cae5"}*/
+/*TMODJS:{"version":403,"md5":"0866e7cc241c98a47c57f4538144cae5"}*/
 template('flow/checkout',function($data,$filename
 /**/) {
 'use strict';var $utils=this,$helpers=$utils.$helpers,$escape=$utils.$escape,data=$data.data,$each=$utils.$each,supplier=$data.supplier,$index=$data.$index,goods=$data.goods,$out='';$out+='<nav class="mui-bar mui-bar-tab mui-row"> <div class="mui-col-xs-7"><a class="mui-tab-item">合计：<span class="color_2fd0b5 orderTotal" data-total="';
